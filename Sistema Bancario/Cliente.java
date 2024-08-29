4d46a37d63f0cff68eef0de504eae6bf41714273
Pedro Lucas de Souza Faria <105456789+Meiamonee@users.noreply.github.com>
2024-08-28 23:10:57 -0300

    Add files via upload

--- Sistema Bancario/Cliente.java
+++ Sistema Bancario/Cliente.java
@@ -1,15 +1,16 @@
-/**
- * Cliente
- */
 public abstract class Cliente {
     private String cpf;
     private String nome;
     private Conta conta;
+    private String login;
+    private String senha;
 
-    public Cliente(String cpf, String nome, Conta conta){
+    public Cliente(String cpf, String nome, Conta conta, String login, String senha) {
         this.cpf = cpf;
         this.nome = nome;
         this.conta = conta;
+        this.login = login;
+        this.senha = senha;
     }
 
     public Conta getConta() {
@@ -23,5 +24,24 @@ public abstract class Cliente {
     public String getNome() {
         return nome;
     }
-    
+
+    public String getLogin() {
+        return login;
+    }
+
+    public boolean validarSenha(String senha) {
+        return this.senha.equals(senha);
+    }
+
+    public void alterarSenha(String novaSenha) {
+        this.senha = novaSenha;
+    }
+
+    public void executarOperacao(String login, String senha, Runnable operacao) {
+        if (this.login.equals(login) && validarSenha(senha)) {
+            operacao.run();
+        } else {
+            System.out.println("Login ou senha incorretos para " + this.nome);
+        }
+    }
 }
