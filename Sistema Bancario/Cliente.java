d19cbde79e8e3e4ff23bbde076567d99274ac8ef
JoaoGSouza <102314073+JoaoGSouza@users.noreply.github.com>
2024-09-04 17:20:08 -0300

    Atualização classe Cliente

--- Sistema Bancario/Cliente.java
+++ Sistema Bancario/Cliente.java
@@ -1,3 +1,10 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public abstract class Cliente {
     private String cpf;
     private String nome;
@@ -20,7 +27,7 @@ public abstract class Cliente {
     public String getCpf() {
         return cpf;
     }
-    
+
     public String getNome() {
         return nome;
     }
@@ -33,8 +40,35 @@ public abstract class Cliente {
         return this.senha.equals(senha);
     }
 
-    public void alterarSenha(String novaSenha) {
-        this.senha = novaSenha;
+    public static Cliente login(String login, String senha) {
+        String query = "SELECT * FROM clientes WHERE login = ? AND senha = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, login);
+            stmt.setString(2, senha);
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                String cpf = rs.getString("cpf");
+                String nome = rs.getString("nome");
+                String numeroConta = rs.getString("numero_conta");
+                float saldo = rs.getFloat("saldo");
+
+                Conta conta;
+                if (rs.getFloat("limite_credito") > 0) {
+                    conta = new ContaCorrente(numeroConta, saldo, nome, rs.getFloat("limite_credito"));
+                } else if (rs.getFloat("taxa_rendimento") > 0) {
+                    conta = new ContaPoupanca(numeroConta, saldo, nome, rs.getFloat("taxa_rendimento"));
+                } else {
+                    conta = new Conta(numeroConta, saldo, nome);
+                }
+
+                return new Cliente(cpf, nome, conta, login, senha) {};
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public void executarOperacao(String login, String senha, Runnable operacao) {
